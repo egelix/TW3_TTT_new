111df70da2f56a358626067ce6e25e9ad3ef6b2b
Manuel2301 <b.manuel23198@gmail.com>
2022-11-14 20:04:06 +0100

    implemented Menu

--- menu.js
+++ menu.js
@@ -1,24 +1,27 @@
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')({ sigint: true });
 module.exports = {
-    getMenuOption: function() {
-        let menu = prompt("hello");
+  getMenuOption: function () {
+    console.log(
+      'Choose from the following options:\n 1. Human vs Human\n 2. Random AI vs Random AI\n 3. Human vs Random AI\n 4. Human vs Unbeatable AI');
+    let menu = prompt();
 
-        while (menu !="1" || menu !="2" || menu !="3" || menu !="4") {
-        console.log("Choose from the following options: 1. Human vs Human\n 2. Random AI vs Random AI\n 3. Human vs Random AI\n 4. Human vs Unbeatable AI")
-        if (menu === "1") {
-            return 1;
-        }
-        else if (menu === "2") {
-            return 2;
-        }
-        else if (menu === "3") {
-            return 3;
-        }
-        else if (menu === "4") {
-            return 4;
-        }
+    while (menu != '1' && menu != '2' && menu != '3' && menu != '4') {
+      console.log('Please enter a number between 1 and 4');
+      console.log(
+        'Choose from the following options:\n 1. Human vs Human\n 2. Random AI vs Random AI\n 3. Human vs Random AI\n 4. Human vs Unbeatable AI');
+      let menu = prompt();
+      if (menu === '1') {
+        return 1;
+      } else if (menu === '2') {
+        return 2;
+      } else if (menu === '3') {
+        return 3;
+      } else if (menu === '4') {
+        return 4;
+      }
     }
-        /*
+
+    /*
         Should print a menu with the following options:
         1. Human vs Human
         2. Random AI vs Random AI
@@ -28,16 +31,13 @@ module.exports = {
         If the user will enter invalid data (for example 5), than a message will appear
         asking to input a new value.
         */
-    }
- }
+  },
+};
 
 // run this function to test whether you have correctly implemented the other function
-function checkOptions()
-{
-    let option = menu.getMenuOption();
-    console.log("If the user selected 1, it should print 1");
-    console.log(option);
+function checkOptions() {
+  let option = module.exports.getMenuOption();
+  console.log(option);
 }
-
 
 checkOptions();
