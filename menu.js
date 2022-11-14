42de4edbf4b3b65fb6229a488db4f4ed839a6e92
Manuel2301 <b.manuel23198@gmail.com>
2022-11-14 14:42:58 +0100

    added Menu

--- menu.js
+++ menu.js
@@ -1,5 +1,23 @@
+const prompt = require("prompt-sync")();
 module.exports = {
     getMenuOption: function() {
+        let menu = prompt("hello");
+
+        while (menu !="1" || menu !="2" || menu !="3" || menu !="4") {
+        console.log("Choose from the following options: 1. Human vs Human\n 2. Random AI vs Random AI\n 3. Human vs Random AI\n 4. Human vs Unbeatable AI")
+        if (menu === "1") {
+            return 1;
+        }
+        else if (menu === "2") {
+            return 2;
+        }
+        else if (menu === "3") {
+            return 3;
+        }
+        else if (menu === "4") {
+            return 4;
+        }
+    }
         /*
         Should print a menu with the following options:
         1. Human vs Human
@@ -16,7 +34,10 @@ module.exports = {
 // run this function to test whether you have correctly implemented the other function
 function checkOptions()
 {
-    let option = getMenuOption();
+    let option = menu.getMenuOption();
     console.log("If the user selected 1, it should print 1");
     console.log(option);
 }
+
+
+checkOptions();
