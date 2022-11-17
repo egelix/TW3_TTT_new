13ae3c9ae4d5d337de97b57343a54e61ab64c3ec
Martin Egert <m.egert82@gmail.com>
2022-11-17 16:12:40 +0100

    unbeatable is unbeatable

--- unbeatable.js
+++ unbeatable.js
@@ -8,21 +8,46 @@ module.exports = {
         return module.exports.botXSecondMove(board, bot, currentPlayer, name2, gameRound);
       } else if (module.exports.takeTheWin(board, bot, currentPlayer, name2, gameRound) !== false) {
         return module.exports.takeTheWin(board, bot, currentPlayer, name2, gameRound);
-      } else {
+      } else if (module.exports.preventLosing(board, bot, currentPlayer, name2, gameRound) !==false) {
         return module.exports.preventLosing(board, bot, currentPlayer, name2, gameRound);
+      } else if (gameRound === 5) {
+        return module.exports.botXThirdMove(board, bot, currentPlayer, name2, gameRound);
       }
     },
     getBotOCoord: function (board, bot, currentPlayer, name2, gameRound) {
         return
     },
-    // botXFirstMove: function () {
-    //     return "22"
-    // },
+ 
     botXSecondMove: function (board, bot, currentPlayer, name2, gameRound) {
         if (board[1][1] === "O") {
             return "00"
-        } return
+        } else if (board[0][1] === "O" || board[0][2] === "O" || board[1][2] === "O") {
+            return "20";
+        } else if (board[1][0] === "O" || board[2][0] === "O" || board[2][1] === "O") {
+            return "02";
+        } else if (board[0][0] === "O") {
+            return "20";
+        }
     },
+    
+    botXThirdMove: function (board, bot, currentPlayer, name2, gameRound) {
+        if (board[0][1] === "O" && board[2][1] === "O" && board[2][0] === "X") {
+            return "11";
+        } else if (board[1][2] === "O" && board[2][1] === "O" && board[2][0] === "X") {
+            return "11";
+        } else if (board[0][2] === "O" && board[2][1] === "O" && board[2][0] === "X") {
+            return "00";
+        } else if (board[0][0] === "O") {
+            return "11";
+        } else if (board[1][0] === "O" && board[1][2] === "O" && board[0][2] === "X") {
+            return "11";
+        } else if (board[2][1] === "O" && board[1][2] === "O" && board[0][2] === "X") {
+            return "11";
+        } else {
+            return "00";
+        }
+    },
+
     takeTheWin: function (board, bot, currentPlayer, name2, gameRound) {
         if (board[0][0] === bot && board[0][1] === bot && board[0][2] === '.') {
             return "02";
