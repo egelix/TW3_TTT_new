e6dfa60f6b8e91eb782183845d4f5d07baf5af43
Manuel2301 <b.manuel23198@gmail.com>
2022-11-17 12:41:06 +0100

    implement unbeatable

--- unbeatable.js
+++ unbeatable.js
@@ -1,111 +1,162 @@
 module.exports = {
     botStartsFirstMove: function () {
         return "22"
-    }
-    botStartsSeconMove: function (board, playerCoord) {
+    },
+    botStartsSecondMove: function (board, playerCoord) {
         if (playerCoord === "11") {
             return "00"
         }
     },
-    chanceToWin: function (board) {
-        if (board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X') {
-            return true;
-          } else if (
-            board[1][0] === 'X' &&
-            board[1][1] === 'X' &&
-            board[1][2] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[2][0] === 'X' &&
-            board[2][1] === 'X' &&
-            board[2][2] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[0][0] === 'X' &&
-            board[1][0] === 'X' &&
-            board[2][0] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[0][1] === 'X' &&
-            board[1][1] === 'X' &&
-            board[2][1] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[0][2] === 'X' &&
-            board[1][2] === 'X' &&
-            board[2][2] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[0][0] === 'X' &&
-            board[1][1] === 'X' &&
-            board[2][2] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[2][0] === 'X' &&
-            board[1][1] === 'X' &&
-            board[0][2] === 'X'
-          ) {
-            return true;
-          } else if (
-            board[0][0] === 'O' &&
-            board[0][1] === 'O' &&
-            board[0][2] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[1][0] === 'O' &&
-            board[1][1] === 'O' &&
-            board[1][2] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[2][0] === 'O' &&
-            board[2][1] === 'O' &&
-            board[2][2] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[0][0] === 'O' &&
-            board[1][0] === 'O' &&
-            board[2][0] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[0][1] === 'O' &&
-            board[1][1] === 'O' &&
-            board[2][1] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[0][2] === 'O' &&
-            board[1][2] === 'O' &&
-            board[2][2] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[0][0] === 'O' &&
-            board[1][1] === 'O' &&
-            board[2][2] === 'O'
-          ) {
-            return true;
-          } else if (
-            board[2][0] === 'O' &&
-            board[1][1] === 'O' &&
-            board[0][2] === 'O'
-          ) {
-            return true;
-          } else {
-            return false;
+    takeTheWin: function (board, bot) {
+        if (board[0][0] === bot && board[0][1] === bot && board[0][2] === '.') {
+            return board[0][2];
+          } 
+          else if (board[0][0] === '.' && board[0][1] === bot && board[0][2] === bot) {
+            return board[0][0];
           }
+          else if (board[0][0] === bot && board[0][1] === '.' && board[0][2] === bot) {
+            return board[0][1];
+          }
+          else if (board[1][0] === bot && board[1][1] === bot && board[1][2] === '.') {
+            return board[1][2];
+          }
+          else if (board[1][0] === bot && board[1][1] === '.' && board[1][2] === bot) {
+            return board[1][1];
+          }
+          else if (board[1][0] === '.' && board[1][1] === bot && board[1][2] === bot) {
+            return board[1][0];
+          }
+          else if (board[2][0] === bot && board[2][1] === bot &&board[2][2] === '.') {
+            return board[2][2];
+          }
+          else if (board[2][0] === bot && board[2][1] === '.' &&board[2][2] === bot) {
+            return board[2][1];
+          }
+          else if (board[2][0] === '.' && board[2][1] === bot &&board[2][2] === bot) {
+            return board[2][0];
+          }
+           else if (board[0][0] === bot && board[1][0] === bot && board[2][0] === '.') {
+            return board[2][0];
+          } 
+          else if (board[0][0] === bot && board[1][0] === '.' && board[2][0] === bot) {
+            return board[1][0];
+          } 
+          else if (board[0][0] === '.' && board[1][0] === bot && board[2][0] === bot) {
+            return board[0][0];
+          } 
+          else if (board[0][1] === bot && board[1][1] === bot && board[2][1] === '.') {
+            return board[2][1];
+          } 
+          else if (board[0][1] === bot && board[1][1] === '.' && board[2][1] === bot) {
+            return board[1][1];
+          }
+          else if (board[0][1] === '.' && board[1][1] === bot && board[2][1] === bot) {
+            return board[0][1];
+          } 
+          else if (board[0][2] === bot && board[1][2] === bot && board[2][2] === '.') {
+            return board[2][2];
+          } 
+          else if (board[0][2] === bot && board[1][2] === '.' && board[2][2] === bot) {
+            return board[1][2];
+          }
+          else if (board[0][2] === '.' && board[1][2] === bot && board[2][2] === bot) {
+            return board[0][2];
+          }  
+          else if (board[0][0] === bot && board[1][1] === bot && board[2][2] === '.') {
+            return board[2][2];
+          } 
+          else if (board[0][0] === bot && board[1][1] === '.' && board[2][2] === bot) {
+            return board[1][1];
+          }
+          else if (board[0][0] === '.' && board[1][1] === bot && board[2][2] === bot) {
+            return board[0][0];
+          } 
+          else if (board[2][0] === bot && board[1][1] === bot && board[0][2] === '.') {
+            return board[0][2];
+          } 
+          else if (board[2][0] === bot && board[1][1] === '.' && board[0][2] === '.') {
+            return board[1][1];
+          }
+          else if (board[2][0] === '.' && board[1][1] === bot && board[0][2] === '.') {
+            return board[2][0];
+          } 
         },
 
-    }
+        preventLosing: function (board, bot) {
+          let opponent = bot === "X" ? "O" : "X"
+          if (board[0][0] === opponent && board[0][1] === opponent && board[0][2] === '.') {
+            return board[0][2];
+          } 
+          else if (board[0][0] === '.' && board[0][1] === opponent && board[0][2] === opponent) {
+            return board[0][0];
+          }
+          else if (board[0][0] === opponent && board[0][1] === '.' && board[0][2] === opponent) {
+            return board[0][1];
+          }
+          else if (board[1][0] === opponent && board[1][1] === opponent && board[1][2] === '.') {
+            return board[1][2];
+          }
+          else if (board[1][0] === opponent && board[1][1] === '.' && board[1][2] === opponent) {
+            return board[1][1];
+          }
+          else if (board[1][0] === '.' && board[1][1] === opponent && board[1][2] === opponent) {
+            return board[1][0];
+          }
+          else if (board[2][0] === opponent && board[2][1] === opponent && board[2][2] === '.') {
+            return board[2][2];
+          }
+          else if (board[2][0] === opponent && board[2][1] === '.' && board[2][2] === opponent) {
+            return board[2][1];
+          }
+          else if (board[2][0] === '.' && board[2][1] === opponent && board[2][2] === opponent) {
+            return board[2][0];
+          }
+           else if (board[0][0] === opponent && board[1][0] === opponent && board[2][0] === '.') {
+            return board[2][0];
+          } 
+          else if (board[0][0] === opponent && board[1][0] === '.' && board[2][0] === opponent) {
+            return board[1][0];
+          } 
+          else if (board[0][0] === '.' && board[1][0] === opponent && board[2][0] === opponent) {
+            return board[0][0];
+          } 
+          else if (board[0][1] === opponent && board[1][1] === opponent && board[2][1] === '.') {
+            return board[2][1];
+          } 
+          else if (board[0][1] === opponent && board[1][1] === '.' && board[2][1] === opponent) {
+            return board[1][1];
+          }
+          else if (board[0][1] === '.' && board[1][1] === opponent && board[2][1] === opponent) {
+            return board[0][1];
+          } 
+          else if (board[0][2] === opponent && board[1][2] === opponent && board[2][2] === '.') {
+            return board[2][2];
+          } 
+          else if (board[0][2] === opponent && board[1][2] === '.' && board[2][2] === opponent) {
+            return board[1][2];
+          }
+          else if (board[0][2] === '.' && board[1][2] === opponent && board[2][2] === opponent) {
+            return board[0][2];
+          }  
+          else if (board[0][0] === opponent && board[1][1] === opponent && board[2][2] === '.') {
+            return board[2][2];
+          } 
+          else if (board[0][0] === opponent && board[1][1] === '.' && board[2][2] === opponent) {
+            return board[1][1];
+          }
+          else if (board[0][0] === '.' && board[1][1] === opponent && board[2][2] === opponent) {
+            return board[0][0];
+          } 
+          else if (board[2][0] === opponent && board[1][1] === opponent && board[0][2] === '.') {
+            return board[0][2];
+          } 
+          else if (board[2][0] === opponent && board[1][1] === '.' && board[0][2] === '.') {
+            return board[1][1];
+          }
+          else if (board[2][0] === '.' && board[1][1] === opponent && board[0][2] === '.') {
+            return board[2][0];
+          } 
+        }
 }
 
    
